9a2ebb9d160748da68d910c114968986cdb32384
Guenther Froestl <gfr@gammadata.de>
2015-11-21 18:20:55 +0100

    test for new xml files

--- taopix_services/src/test/java/at/cyberlab/taopix_services/imports/TaopixNeuXmlParserTest.java
+++ taopix_services/src/test/java/at/cyberlab/taopix_services/imports/TaopixNeuXmlParserTest.java
@@ -9,22 +9,43 @@ import at.cyberlab.taopix_services.config.TaopixTomImportConfigImpl;
 import com.tom.service.dto.AddressDTO;
 import com.tom.service.dto.BelegPositionDTO;
 import java.io.InputStream;
+import java.util.Arrays;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  *
  * @author gfr
  */
+@RunWith(value = Parameterized.class)
 public class TaopixNeuXmlParserTest {
 
-  TaopixTomImportConfig taopixTomConfig = new TaopixTomImportConfigImpl();
+  TaopixTomImportConfig taopixTomConfig;
+  private String inputFilePath;
+  private Integer expectedOrderNumber;
 
-  public TaopixNeuXmlParserTest() {
+  public TaopixNeuXmlParserTest(String inputFilePath, Integer expectedOrderNumber) {
+    this.inputFilePath = inputFilePath;
+    this.expectedOrderNumber = expectedOrderNumber;
+  }
+
+  @Parameters()
+  public static Iterable<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {"lamprechter/1090649.xml", 1090649},
+      {"lamprechter/1091046.xml", 1091046},
+      {"lamprechter/1092947.xml", 1092947},
+      {"lamprechter/1093390.xml", 1093390},
+      {"lamprechter/1093390.xml", 1093390},}
+    );
   }
 
   @BeforeClass
@@ -37,6 +58,7 @@ public class TaopixNeuXmlParserTest {
 
   @Before
   public void setUp() {
+    taopixTomConfig = new TaopixTomImportConfigImpl();
   }
 
   @After
@@ -49,25 +71,23 @@ public class TaopixNeuXmlParserTest {
   @Test
   public void testParseTaopixOrder() throws Exception {
     System.out.println("parse");
-    InputStream xmlStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("taopix/taopix_neu.xml");
-
+    InputStream xmlStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(this.inputFilePath);
 
     TaopixToTomXmlParser instance = new TaopixToTomXmlParser(taopixTomConfig);
     instance.parse(xmlStream);
     xmlStream.close();
     assertNotNull("order == null", instance.getOrder());
     assertEquals("Mandator ID stimmt nicht ", taopixTomConfig.getMandatorId(), instance.getOrder().getMandant());
-    Integer tomOrderNr = taopixTomConfig.getOrderNumberOffset()+Integer.parseInt(instance.getOrderId());
-//    assertEquals("TomOrderNr stimmt nicht ", new Integer(112505272), tomOrderNr);
+    Integer tomOrderNr = Integer.parseInt(instance.getOrderId());
+    assertEquals("TomOrderNr stimmt nicht ", expectedOrderNumber, tomOrderNr);
     System.out.println("Order= " + instance.getOrder());
 
-    
     AddressDTO address = instance.getOrderAddress();
     System.out.println("Orderaddress= " + address);
 
     AddressDTO shipaddress = instance.getShippingAddress();
     System.out.println("Shippingaddress=" + shipaddress);
-    
+
     BelegPositionDTO pos = instance.getPosition();
     System.out.println("Product=" + pos);
 
@@ -76,7 +96,6 @@ public class TaopixNeuXmlParserTest {
 
     BelegPositionDTO discount = instance.getDiscount();
     System.out.println("Discount=" + discount);
-
 
   }
 
--- taopix_services/src/test/java/at/cyberlab/taopix_services/imports/processing/PdfGenerateAndPrintProcessorTest.java
+++ taopix_services/src/test/java/at/cyberlab/taopix_services/imports/processing/PdfGenerateAndPrintProcessorTest.java
@@ -11,7 +11,7 @@ import at.cyberlab.taopix_services.imports.TaopixToTomXmlParser;
 import de.gammadata.tom.four_d_access.dataBase.DataBaseSpec;
 import java.io.File;
 import java.io.InputStream;
-import java.net.URISyntaxException;
+import java.util.Arrays;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -19,19 +19,37 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  *
  * @author gfr
  */
+@RunWith(value = Parameterized.class)
 public class PdfGenerateAndPrintProcessorTest {
 
   private static Logger LOG = Logger.getLogger(PdfGenerateAndPrintProcessorTest.class);
   private CyberlabConfigProviderImpl provider = new CyberlabConfigProviderImpl();
   private TaopixTomImportConfig config = new TaopixTomImportConfigImpl();
   private PdfGenerateProcessor testee;
+  private String inputFilePath;
+  private Integer expectedOrderNumber;
 
-  public PdfGenerateAndPrintProcessorTest() {
+  public PdfGenerateAndPrintProcessorTest(String inputFilePath, Integer expectedOrderNumber) {
+    this.inputFilePath = inputFilePath;
+    this.expectedOrderNumber = expectedOrderNumber;
+  }
+
+  @Parameterized.Parameters()
+  public static Iterable<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+      {"lamprechter/1090649.xml", 1090649},
+      {"lamprechter/1091046.xml", 1091046},
+      {"lamprechter/1092947.xml", 1092947},
+      {"lamprechter/1093390.xml", 1093390},
+      {"lamprechter/1093390.xml", 1093390},}
+    );
   }
 
   @BeforeClass
@@ -64,14 +82,11 @@ public class PdfGenerateAndPrintProcessorTest {
     System.out.println("Using xsl-file=" + url);
     File xslFile = new File(url.toURI());
     testee.setXsl(xslFile);
-    InputStream xmlStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("taopix/0005343.xml");
+    InputStream xmlStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(this.inputFilePath);
     TaopixToTomXmlParser instance = new TaopixToTomXmlParser(new TaopixTomImportConfigImpl());
     instance.parse(xmlStream);
     xmlStream.close();
 
-    assertNotNull("TaopixOrder == null", instance.getOrder());
-    assertNotNull("Positionsliste == null", instance.getOrder().getPositionsListe());
-    assertEquals("Positionsliste != 3", 3, instance.getOrder().getPositionsListe().size());
     TaopixImportProcessingObject processionObj = new TaopixImportProcessingObject();
     processionObj.setTaopixOrder(instance.getOrder());
     testee.processOrder(processionObj);
